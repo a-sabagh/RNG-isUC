d97005cfe516b01169e82304af02dbdd27f3c8a5
asabagh <a.sabagh72@gmail.com>
2018-06-14 11:56:33 -0700

    adding unistall proccess

--- uninstall.php
+++ uninstall.php
@@ -1,2 +1,15 @@
 <?php
 defined( 'WP_UNINSTALL_PLUGIN' ) || exit;
+//delte options
+$options = array(
+    //settings
+    "uc_settings",
+    "uc_configration_dissmiss",
+    //widget
+    "widget_uc-post-viewed"
+);
+foreach ($options as $option) {
+    if (get_option($option)) {
+        delete_option($option);
+    }
+}
